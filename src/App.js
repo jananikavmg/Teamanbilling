62669965e1a7ccadb06e24414cad9109652fcf17
jananikavmg <121153940+jananikavmg@users.noreply.github.com>
2023-04-02 11:17:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,225 @@
+import { useState, useRef, useEffect } from "react";
+import ClientDetails from "../src/components/ClientDetails";
+import Dates from "../src/components/Dates";
+import Footer from "../src/components/Footer";
+import Header from "../src/components/Header";
+import MainDetails from "../src/components/MainDetails";
+import Notes from "../src/components/Notes";
+import Table from "../src/components/Table";
+import TableForm from "../src/components/TableForm";
+import ReactToPrint from "react-to-print";
+
+function App() {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [bankName, setBankName] = useState("");
+  const [bankAccount, setBankAccount] = useState("");
+  const [website, setWebsite] = useState("");
+  const [clientName, setClientName] = useState("");
+  const [clientAddress, setClientAddress] = useState("");
+  const [invoiceNumber, setInvoiceNumber] = useState("");
+  const [invoiceDate, setInvoiceDate] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [notes, setNotes] = useState("");
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+  const [amount, setAmount] = useState("");
+  const [list, setList] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [width] = useState(641);
+  // const [invoices, setInvoices] = useState([]);
+
+  const componentRef = useRef();
+
+  const handlePrint = () => {
+    window.print();
+  };
+
+  useEffect(() => {
+    if (window.innerWidth < width) {
+      alert("Place your phone in landscape mode for the best experience");
+    }
+  }, [width]);
+
+  return (
+    <>
+      <main
+        className="m-5 p-5 xl:grid grid-cols-2 gap-10 xl:items-start"
+        style={{
+          maxWidth: "1920px",
+          margin: "auto",
+        }}
+      >
+        <section>
+          <div className="bg-white p-5 rounded shadow">
+            {/* name, address, email, phone, bank name, bank account number, website client name, client address, invoice number, invoice date, due date, notes */}
+            <div className="flex flex-col justify-center">
+              <article className="md:grid grid-cols-2 gap-10 md:mt-16">
+                <div className="flex flex-col">
+                  <label htmlFor="clientName">Enter your client's name</label>
+                  <input
+                    type="text"
+                    name="clientName"
+                    id="clientName"
+                    placeholder="Enter your client's name"
+                    autoComplete="off"
+                    value={clientName}
+                    onChange={(e) => setClientName(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="clientAddress">
+                    Enter your client's address
+                  </label>
+                  <input
+                    type="text"
+                    name="clientAddress"
+                    id="clientAddress"
+                    placeholder="Enter your client's address"
+                    autoComplete="off"
+                    value={clientAddress}
+                    onChange={(e) => setClientAddress(e.target.value)}
+                  />
+                </div>
+              </article>
+
+              <article className="md:grid grid-cols-3 gap-10">
+                <div className="flex flex-col">
+                  <label htmlFor="invoiceNumber">Invoice Number</label>
+                  <input
+                    type="text"
+                    name="invoiceNumber"
+                    id="invoiceNumber"
+                    placeholder="Invoice Number"
+                    autoComplete="off"
+                    value={invoiceNumber}
+                    onChange={(e) => setInvoiceNumber(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="invoiceDate">Invoice Date</label>
+                  <input
+                    type="date"
+                    name="invoiceDate"
+                    id="invoiceDate"
+                    placeholder="Invoice Date"
+                    autoComplete="off"
+                    value={invoiceDate}
+                    onChange={(e) => setInvoiceDate(e.target.value)}
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="dueDate">Timing</label>
+                  <select
+                    onChange={(e) => setDueDate(e.target.value)}
+                    name="DueDate"
+                  >
+                    <option defaultValue disabled>
+                      Select Timing
+                    </option>
+                    <option value="Morning">Morning</option>
+                    <option value="Evening">Evening</option>
+                  </select>
+                </div>
+              </article>
+
+              {/* This is our table form */}
+              <article>
+                <TableForm
+                  description={description}
+                  setDescription={setDescription}
+                  quantity={quantity}
+                  setQuantity={setQuantity}
+                  price={price}
+                  setPrice={setPrice}
+                  amount={amount}
+                  setAmount={setAmount}
+                  list={list}
+                  setList={setList}
+                  total={total}
+                  setTotal={setTotal}
+                />
+              </article>
+
+              <label htmlFor="notes">Additional Notes</label>
+              <textarea
+                name="notes"
+                id="notes"
+                cols="30"
+                rows="10"
+                placeholder="Additional notes to the client"
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)}
+              ></textarea>
+            </div>
+          </div>
+        </section>
+
+        {/* Invoice Preview */}
+        <div className="invoice__preview bg-white p-5 rounded">
+          <ReactToPrint
+            trigger={() => (
+              <button className="bg-blue-500 ml-5 text-white font-bold py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300">
+                Print / Download
+              </button>
+            )}
+            content={() => componentRef.current}
+          />
+          <div ref={componentRef} className="p-5">
+            <Header handlePrint={handlePrint} />
+
+            <MainDetails name={name} address={address} />
+
+            <ClientDetails
+              clientName={clientName}
+              clientAddress={clientAddress}
+            />
+
+            <Dates
+              invoiceNumber={invoiceNumber}
+              invoiceDate={invoiceDate}
+              dueDate={dueDate}
+            />
+
+            <Table
+              description={description}
+              quantity={quantity}
+              price={price}
+              amount={amount}
+              list={list}
+              setList={setList}
+              total={total}
+              setTotal={setTotal}
+            />
+
+            <Notes notes={notes} />
+
+            <Footer
+              name={name}
+              address={address}
+              website={website}
+              email={email}
+              phone={phone}
+              bankAccount={bankAccount}
+              bankName={bankName}
+            />
+          </div>
+          {/* <button
+            onClick={() => setShowInvoice(false)}
+            className="mt-5 bg-blue-500 text-white font-bold py-2 px-8 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 transition-all duration-300"
+          >
+            Edit Information
+          </button> */}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer({
+ 
+}) {
+  return (
+    <>
+      <footer className="footer border-t-2 border-gray-300 pt-5">
+        <h2 className="font-bold" style={{textAlign:"center"}}>Thanks for visting us!</h2>
+      </footer>
+    </>
+  )
+}
